965159dd5a1eaf96c0ca73c3ad941a0e149c692f
crazyhuiliang <94huiliang@gmail.com>
2022-02-28 15:14:35 +0800

    t105-2 comment

--- 10/t105-2.asm
+++ 10/t105-2.asm
@@ -9,12 +9,12 @@ start:  mov ax,data
         mov sp,16
         mov word ptr ss:[0],offset s    ; 标号s的地址（偏移地址）
         mov ss:[2],cs                   ; 代码执行的保存段地址
-        call dword ptr ss:[0]           ; 转移至s，压栈下一行nop的段地址（0C5A）和偏移地址
+        call dword ptr ss:[0]           ; 转移至s，压栈下一行nop的段地址（0C5aH）ss:[0eH] 和偏移地址(0019H) ss:[0cH]
         nop
-s:      mov ax,offset s                 ; 标号s的地址（偏移地址）
-        sub ax,ss:[0cH]                 ; 减去
-        mov bx,cs
-        sub bx,ss:[0eH]
+s:      mov ax,offset s                 ; 标号s的地址(偏移地址 001aH）
+        sub ax,ss:[0cH]                 ; 减去 0019H = 1 <------------------- ax结果
+        mov bx,cs                       ; 段地址 0c5aH
+        sub bx,ss:[0eH]                 ; 减去 0c5aH = 0 <------------------- bx结果
 
         mov ax,4c00H
         int 21H
