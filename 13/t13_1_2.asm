b43c979f98df456553b7d3a97e9d26a172642cae
HuiLiang <CrazyHuiLiang@163.com>
2022-03-30 19:06:05 +0800

    fix: miss paramater

--- 13/t13_1_2.asm
+++ 13/t13_1_2.asm
@@ -30,7 +30,7 @@ start:
 ; 中断程序
 jpn:
     push bp ; 使用bp作为偏移地址，配合
-    mov bp,
+    mov bp,sp
     ; int 7cH中断触发后，当前的标志寄存器、CS和IP都要压栈，
     ; 此时压入的CS和IP的内容，分别是调用程序的段地址和int 7cH后一条指令的偏移地址（即标号se的偏移地址）
     ; 我们可以直接从栈中我们将栈中标号se的偏移地址加上bx中的转移位移，则栈中的se的偏移地址就变成了s的偏移地址；
@@ -61,9 +61,4 @@ ok: mov ax,4c00H
     int 21H
 code ends
 end start
-
-0b44:0040
-
-ss: 0b43
-sp: 0
 
